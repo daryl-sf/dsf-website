86e07a0e1c420be9a0f14c49a5055eacee00d08d
Daryl Findlay <darylfindlay@gmail.com>
2021-11-19 00:21:48 +0000

    github sign in

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
 import Head from 'next/head'
+import { signIn, useSession } from "next-auth/react"
 import styles from '../styles/Home.module.css'
 
-const Home = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Daryl</title>
-      <meta name="description" content="Hobby website for Daryl Findlay" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = () => {
+  const { data: session, status } = useSession();
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Daryl</title>
+        <meta name="description" content="Hobby website for Daryl Findlay" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to Daryls hobby website!
-      </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to Daryls hobby website!
+        </h1>
 
-      <p className={styles.description}>
-        This is the start of a hobby website for Daryl. The goal is to learn 👨🏼‍🎓
-      </p>
-    </main>
-  </div>
-);
+        <p className={styles.description}>
+          This is the start of a hobby website for Daryl. The goal is to learn 👨🏼‍🎓
+        </p>
+        <p>
+          {status === "authenticated" && "Welcome"}
+          {status !== "authenticated" && <button onClick={() => signIn()}>Sign in</button>}
+        </p>
+      </main>
+    </div>
+  );
+}
 
 export default Home;
